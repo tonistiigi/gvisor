5cbef0c8ec3d43b8401bd6013d186b481fd55bb2
Michael Pratt <mpratt@google.com>
2019-01-31 17:47:24 -0800

    Move package sync to third_party
    PiperOrigin-RevId: 231889261
    Change-Id: I482f1df055bcedf4edb9fe3fe9b8e9c80085f1a0
    Upstream-commit: fe1369ac98a4f1d8af5e8be6da71165339e52034

--- pkg/sentry/kernel/seqatomic_taskgoroutineschedinfo.go
+++ pkg/sentry/kernel/seqatomic_taskgoroutineschedinfo.go
@@ -6,20 +6,20 @@ import (
 	"strings"
 	"unsafe"
 
-	ssync "gvisor.googlesource.com/gvisor/pkg/sync"
+	"gvisor.googlesource.com/gvisor/third_party/gvsync"
 )
 
 // SeqAtomicLoad returns a copy of *ptr, ensuring that the read does not race
 // with any writer critical sections in sc.
-func SeqAtomicLoadTaskGoroutineSchedInfo(sc *ssync.SeqCount, ptr *TaskGoroutineSchedInfo) TaskGoroutineSchedInfo {
+func SeqAtomicLoadTaskGoroutineSchedInfo(sc *gvsync.SeqCount, ptr *TaskGoroutineSchedInfo) TaskGoroutineSchedInfo {
 	// This function doesn't use SeqAtomicTryLoad because doing so is
 	// measurably, significantly (~20%) slower; Go is awful at inlining.
 	var val TaskGoroutineSchedInfo
 	for {
 		epoch := sc.BeginRead()
-		if ssync.RaceEnabled {
+		if gvsync.RaceEnabled {
 
-			ssync.Memmove(unsafe.Pointer(&val), unsafe.Pointer(ptr), unsafe.Sizeof(val))
+			gvsync.Memmove(unsafe.Pointer(&val), unsafe.Pointer(ptr), unsafe.Sizeof(val))
 		} else {
 
 			val = *ptr
@@ -35,10 +35,10 @@ func SeqAtomicLoadTaskGoroutineSchedInfo(sc *ssync.SeqCount, ptr *TaskGoroutineS
 // in sc initiated by a call to sc.BeginRead() that returned epoch. If the read
 // would race with a writer critical section, SeqAtomicTryLoad returns
 // (unspecified, false).
-func SeqAtomicTryLoadTaskGoroutineSchedInfo(sc *ssync.SeqCount, epoch ssync.SeqCountEpoch, ptr *TaskGoroutineSchedInfo) (TaskGoroutineSchedInfo, bool) {
+func SeqAtomicTryLoadTaskGoroutineSchedInfo(sc *gvsync.SeqCount, epoch gvsync.SeqCountEpoch, ptr *TaskGoroutineSchedInfo) (TaskGoroutineSchedInfo, bool) {
 	var val TaskGoroutineSchedInfo
-	if ssync.RaceEnabled {
-		ssync.Memmove(unsafe.Pointer(&val), unsafe.Pointer(ptr), unsafe.Sizeof(val))
+	if gvsync.RaceEnabled {
+		gvsync.Memmove(unsafe.Pointer(&val), unsafe.Pointer(ptr), unsafe.Sizeof(val))
 	} else {
 		val = *ptr
 	}
@@ -49,7 +49,7 @@ func initTaskGoroutineSchedInfo() {
 	var val TaskGoroutineSchedInfo
 	typ := reflect.TypeOf(val)
 	name := typ.Name()
-	if ptrs := ssync.PointersInType(typ, name); len(ptrs) != 0 {
+	if ptrs := gvsync.PointersInType(typ, name); len(ptrs) != 0 {
 		panic(fmt.Sprintf("SeqAtomicLoad<%s> is invalid since values %s of type %s contain pointers:\n%s", typ, name, typ, strings.Join(ptrs, "\n")))
 	}
 }
--- pkg/sentry/time/seqatomic_parameters.go
+++ pkg/sentry/time/seqatomic_parameters.go
@@ -6,20 +6,20 @@ import (
 	"strings"
 	"unsafe"
 
-	ssync "gvisor.googlesource.com/gvisor/pkg/sync"
+	"gvisor.googlesource.com/gvisor/third_party/gvsync"
 )
 
 // SeqAtomicLoad returns a copy of *ptr, ensuring that the read does not race
 // with any writer critical sections in sc.
-func SeqAtomicLoadParameters(sc *ssync.SeqCount, ptr *Parameters) Parameters {
+func SeqAtomicLoadParameters(sc *gvsync.SeqCount, ptr *Parameters) Parameters {
 	// This function doesn't use SeqAtomicTryLoad because doing so is
 	// measurably, significantly (~20%) slower; Go is awful at inlining.
 	var val Parameters
 	for {
 		epoch := sc.BeginRead()
-		if ssync.RaceEnabled {
+		if gvsync.RaceEnabled {
 
-			ssync.Memmove(unsafe.Pointer(&val), unsafe.Pointer(ptr), unsafe.Sizeof(val))
+			gvsync.Memmove(unsafe.Pointer(&val), unsafe.Pointer(ptr), unsafe.Sizeof(val))
 		} else {
 
 			val = *ptr
@@ -35,10 +35,10 @@ func SeqAtomicLoadParameters(sc *ssync.SeqCount, ptr *Parameters) Parameters {
 // in sc initiated by a call to sc.BeginRead() that returned epoch. If the read
 // would race with a writer critical section, SeqAtomicTryLoad returns
 // (unspecified, false).
-func SeqAtomicTryLoadParameters(sc *ssync.SeqCount, epoch ssync.SeqCountEpoch, ptr *Parameters) (Parameters, bool) {
+func SeqAtomicTryLoadParameters(sc *gvsync.SeqCount, epoch gvsync.SeqCountEpoch, ptr *Parameters) (Parameters, bool) {
 	var val Parameters
-	if ssync.RaceEnabled {
-		ssync.Memmove(unsafe.Pointer(&val), unsafe.Pointer(ptr), unsafe.Sizeof(val))
+	if gvsync.RaceEnabled {
+		gvsync.Memmove(unsafe.Pointer(&val), unsafe.Pointer(ptr), unsafe.Sizeof(val))
 	} else {
 		val = *ptr
 	}
@@ -49,7 +49,7 @@ func initParameters() {
 	var val Parameters
 	typ := reflect.TypeOf(val)
 	name := typ.Name()
-	if ptrs := ssync.PointersInType(typ, name); len(ptrs) != 0 {
+	if ptrs := gvsync.PointersInType(typ, name); len(ptrs) != 0 {
 		panic(fmt.Sprintf("SeqAtomicLoad<%s> is invalid since values %s of type %s contain pointers:\n%s", typ, name, typ, strings.Join(ptrs, "\n")))
 	}
 }
