22d7588d6d972e38b5dc2c15aca7df5580ed229c
Kevin Krakauer <krakauer@google.com>
2019-02-27 14:30:20 -0800

    Ping support via IPv4 raw sockets.
    Broadly, this change:
    * Enables sockets to be created via `socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)`.
    * Passes the network-layer (IP) header up the stack to the transport endpoint,
      which can pass it up to the socket layer. This allows a raw socket to return
      the entire IP packet to users.
    * Adds functions to stack.TransportProtocol, stack.Stack, stack.transportDemuxer
      that enable incoming packets to be delivered to raw endpoints. New raw sockets
      of other protocols (not ICMP) just need to register with the stack.
    * Enables ping.endpoint to return IP headers when created via SOCK_RAW.
    
    PiperOrigin-RevId: 235993280
    Change-Id: I60ed994f5ff18b2cbd79f063a7fdf15d093d845a
    Upstream-commit: 121db29a93c651b8b62e8701bb0f16c231b08257

--- pkg/tcpip/transport/icmp/icmp_state_autogen.go
+++ pkg/tcpip/transport/icmp/icmp_state_autogen.go
@@ -25,6 +25,47 @@ func (x *icmpPacket) load(m state.Map) {
 	m.LoadValue("data", new(buffer.VectorisedView), func(y interface{}) { x.loadData(y.(buffer.VectorisedView)) })
 }
 
+func (x *endpoint) save(m state.Map) {
+	x.beforeSave()
+	var rcvBufSizeMax int = x.saveRcvBufSizeMax()
+	m.SaveValue("rcvBufSizeMax", rcvBufSizeMax)
+	m.Save("netProto", &x.netProto)
+	m.Save("transProto", &x.transProto)
+	m.Save("waiterQueue", &x.waiterQueue)
+	m.Save("raw", &x.raw)
+	m.Save("rcvReady", &x.rcvReady)
+	m.Save("rcvList", &x.rcvList)
+	m.Save("rcvBufSize", &x.rcvBufSize)
+	m.Save("rcvClosed", &x.rcvClosed)
+	m.Save("sndBufSize", &x.sndBufSize)
+	m.Save("shutdownFlags", &x.shutdownFlags)
+	m.Save("id", &x.id)
+	m.Save("state", &x.state)
+	m.Save("bindNICID", &x.bindNICID)
+	m.Save("bindAddr", &x.bindAddr)
+	m.Save("regNICID", &x.regNICID)
+}
+
+func (x *endpoint) load(m state.Map) {
+	m.Load("netProto", &x.netProto)
+	m.Load("transProto", &x.transProto)
+	m.Load("waiterQueue", &x.waiterQueue)
+	m.Load("raw", &x.raw)
+	m.Load("rcvReady", &x.rcvReady)
+	m.Load("rcvList", &x.rcvList)
+	m.Load("rcvBufSize", &x.rcvBufSize)
+	m.Load("rcvClosed", &x.rcvClosed)
+	m.Load("sndBufSize", &x.sndBufSize)
+	m.Load("shutdownFlags", &x.shutdownFlags)
+	m.Load("id", &x.id)
+	m.Load("state", &x.state)
+	m.Load("bindNICID", &x.bindNICID)
+	m.Load("bindAddr", &x.bindAddr)
+	m.Load("regNICID", &x.regNICID)
+	m.LoadValue("rcvBufSizeMax", new(int), func(y interface{}) { x.loadRcvBufSizeMax(y.(int)) })
+	m.AfterLoad(x.afterLoad)
+}
+
 func (x *icmpPacketList) beforeSave() {}
 func (x *icmpPacketList) save(m state.Map) {
 	x.beforeSave()
@@ -53,6 +94,7 @@ func (x *icmpPacketEntry) load(m state.Map) {
 
 func init() {
 	state.Register("icmp.icmpPacket", (*icmpPacket)(nil), state.Fns{Save: (*icmpPacket).save, Load: (*icmpPacket).load})
+	state.Register("icmp.endpoint", (*endpoint)(nil), state.Fns{Save: (*endpoint).save, Load: (*endpoint).load})
 	state.Register("icmp.icmpPacketList", (*icmpPacketList)(nil), state.Fns{Save: (*icmpPacketList).save, Load: (*icmpPacketList).load})
 	state.Register("icmp.icmpPacketEntry", (*icmpPacketEntry)(nil), state.Fns{Save: (*icmpPacketEntry).save, Load: (*icmpPacketEntry).load})
 }
