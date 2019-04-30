bc2cbe8b84f20c5b7a13024c439da8d4c3d086c9
Jamie Liu <jamieliu@google.com>
2019-04-30 13:55:41 -0700

    Implement async MemoryFile eviction, and use it in CachingInodeOperations.
    This feature allows MemoryFile to delay eviction of "optional"
    allocations, such as unused cached file pages.
    
    Note that this incidentally makes CachingInodeOperations writeback
    asynchronous, in the sense that it doesn't occur until eviction; this is
    necessary because between when a cached page becomes evictable and when
    it's evicted, file writes (via CachingInodeOperations.Write) may dirty
    the page.
    
    As currently implemented, this feature won't meaningfully impact
    steady-state memory usage or caching; the reclaimer goroutine will
    schedule eviction as soon as it runs out of other work to do. Future CLs
    increase caching by adding constraints on when eviction is scheduled.
    
    PiperOrigin-RevId: 246014822
    Change-Id: Ia85feb25a2de92a48359eb84434b6ec6f9bea2cb
    Upstream-commit: 8bfb83d0acdea553082b897d3fd0ad1c1580eaa9

--- pkg/sentry/pgalloc/pgalloc_state_autogen.go
+++ pkg/sentry/pgalloc/pgalloc_state_autogen.go
@@ -6,6 +6,69 @@ import (
 	"gvisor.googlesource.com/gvisor/pkg/state"
 )
 
+func (x *EvictableRange) beforeSave() {}
+func (x *EvictableRange) save(m state.Map) {
+	x.beforeSave()
+	m.Save("Start", &x.Start)
+	m.Save("End", &x.End)
+}
+
+func (x *EvictableRange) afterLoad() {}
+func (x *EvictableRange) load(m state.Map) {
+	m.Load("Start", &x.Start)
+	m.Load("End", &x.End)
+}
+
+func (x *evictableRangeSet) beforeSave() {}
+func (x *evictableRangeSet) save(m state.Map) {
+	x.beforeSave()
+	var root *evictableRangeSegmentDataSlices = x.saveRoot()
+	m.SaveValue("root", root)
+}
+
+func (x *evictableRangeSet) afterLoad() {}
+func (x *evictableRangeSet) load(m state.Map) {
+	m.LoadValue("root", new(*evictableRangeSegmentDataSlices), func(y interface{}) { x.loadRoot(y.(*evictableRangeSegmentDataSlices)) })
+}
+
+func (x *evictableRangenode) beforeSave() {}
+func (x *evictableRangenode) save(m state.Map) {
+	x.beforeSave()
+	m.Save("nrSegments", &x.nrSegments)
+	m.Save("parent", &x.parent)
+	m.Save("parentIndex", &x.parentIndex)
+	m.Save("hasChildren", &x.hasChildren)
+	m.Save("keys", &x.keys)
+	m.Save("values", &x.values)
+	m.Save("children", &x.children)
+}
+
+func (x *evictableRangenode) afterLoad() {}
+func (x *evictableRangenode) load(m state.Map) {
+	m.Load("nrSegments", &x.nrSegments)
+	m.Load("parent", &x.parent)
+	m.Load("parentIndex", &x.parentIndex)
+	m.Load("hasChildren", &x.hasChildren)
+	m.Load("keys", &x.keys)
+	m.Load("values", &x.values)
+	m.Load("children", &x.children)
+}
+
+func (x *evictableRangeSegmentDataSlices) beforeSave() {}
+func (x *evictableRangeSegmentDataSlices) save(m state.Map) {
+	x.beforeSave()
+	m.Save("Start", &x.Start)
+	m.Save("End", &x.End)
+	m.Save("Values", &x.Values)
+}
+
+func (x *evictableRangeSegmentDataSlices) afterLoad() {}
+func (x *evictableRangeSegmentDataSlices) load(m state.Map) {
+	m.Load("Start", &x.Start)
+	m.Load("End", &x.End)
+	m.Load("Values", &x.Values)
+}
+
 func (x *usageInfo) beforeSave() {}
 func (x *usageInfo) save(m state.Map) {
 	x.beforeSave()
@@ -72,6 +135,10 @@ func (x *usageSegmentDataSlices) load(m state.Map) {
 }
 
 func init() {
+	state.Register("pgalloc.EvictableRange", (*EvictableRange)(nil), state.Fns{Save: (*EvictableRange).save, Load: (*EvictableRange).load})
+	state.Register("pgalloc.evictableRangeSet", (*evictableRangeSet)(nil), state.Fns{Save: (*evictableRangeSet).save, Load: (*evictableRangeSet).load})
+	state.Register("pgalloc.evictableRangenode", (*evictableRangenode)(nil), state.Fns{Save: (*evictableRangenode).save, Load: (*evictableRangenode).load})
+	state.Register("pgalloc.evictableRangeSegmentDataSlices", (*evictableRangeSegmentDataSlices)(nil), state.Fns{Save: (*evictableRangeSegmentDataSlices).save, Load: (*evictableRangeSegmentDataSlices).load})
 	state.Register("pgalloc.usageInfo", (*usageInfo)(nil), state.Fns{Save: (*usageInfo).save, Load: (*usageInfo).load})
 	state.Register("pgalloc.usageSet", (*usageSet)(nil), state.Fns{Save: (*usageSet).save, Load: (*usageSet).load})
 	state.Register("pgalloc.usagenode", (*usagenode)(nil), state.Fns{Save: (*usagenode).save, Load: (*usagenode).load})
