330c9847182ef54c68f7841d65aeca5df93421ec
Adin Scannell <ascannell@google.com>
2019-05-21 15:17:05 -0700

    Add basic plumbing for splice and stub implementation.
    This does not actually implement an efficient splice or sendfile. Rather, it
    adds a generic plumbing to the file internals so that this can be added. All
    file implementations use the stub fileutil.NoSplice implementation, which
    causes sendfile and splice to fall back to an internal copy.
    
    A basic splice system call interface is added, along with a test.
    
    PiperOrigin-RevId: 249335960
    Change-Id: Ic5568be2af0a505c19e7aec66d5af2480ab0939b
    Upstream-commit: 9cdae51feca5cee9faa198161b92a0aeece52d6c

--- pkg/sentry/kernel/epoll/epoll_state_autogen.go
+++ pkg/sentry/kernel/epoll/epoll_state_autogen.go
@@ -47,8 +47,8 @@ func (x *EventPoll) save(m state.Map) {
 	if !state.IsZeroValue(x.FileNotDirReaddir) { m.Failf("FileNotDirReaddir is %v, expected zero", x.FileNotDirReaddir) }
 	if !state.IsZeroValue(x.FileNoFsync) { m.Failf("FileNoFsync is %v, expected zero", x.FileNoFsync) }
 	if !state.IsZeroValue(x.FileNoopFlush) { m.Failf("FileNoopFlush is %v, expected zero", x.FileNoopFlush) }
-	if !state.IsZeroValue(x.FileNoMMap) { m.Failf("FileNoMMap is %v, expected zero", x.FileNoMMap) }
 	if !state.IsZeroValue(x.FileNoIoctl) { m.Failf("FileNoIoctl is %v, expected zero", x.FileNoIoctl) }
+	if !state.IsZeroValue(x.FileNoMMap) { m.Failf("FileNoMMap is %v, expected zero", x.FileNoMMap) }
 	if !state.IsZeroValue(x.Queue) { m.Failf("Queue is %v, expected zero", x.Queue) }
 	m.Save("files", &x.files)
 	m.Save("readyList", &x.readyList)
